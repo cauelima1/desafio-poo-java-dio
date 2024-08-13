92e5847053c4decba40df51b285e7907c6110788
cauelima1 <cauezak@gmail.com>
2024-08-13 11:55:25 -0300

    Desafio POO Java Dio concluido
    Signed-off-by: cauelima1 <cauezak@gmail.com>

--- src/Dev.java
+++ src/Dev.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Dev {
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp (Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp(){
+        return this.conteudoConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudoConcluidos() {
+        return conteudoConcluidos;
+    }
+
+    public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudoConcluidos = conteudoConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos()) && Objects.equals(getConteudoConcluidos(), dev.getConteudoConcluidos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudosInscritos(), getConteudoConcluidos());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,42 @@ public class Main {
         mentoria.setDescricao("Descricao mentoria em Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Boot Camp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCaue = new Dev ();
+        devCaue.setNome("Cauê");
+        devCaue.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Caue" + devCaue.getConteudosInscritos());
+        devCaue.progredir();
+        devCaue.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Caue" + devCaue.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Caue" + devCaue.getConteudoConcluidos());
+        System.out.println("XP" + devCaue.calcularTotalXp());
+
+
+        System.out.println("--------------");
+
+        Dev devJose = new Dev();
+        devJose.setNome("José Felipe");
+        devJose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Jose" + devJose.getConteudosInscritos());
+        devJose.progredir();
+        devCaue.progredir();
+        devCaue.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Jose" + devJose.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Jose" + devJose.getConteudoConcluidos());
+        System.out.println("XP" + devJose.calcularTotalXp());
 
 
 
